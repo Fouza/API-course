a6dd044131e17222d9ae0b7a20a56ca91c0e14b9
Fouza <gf_oukacha@esi.dz>
2024-07-25 17:15:45 +0100

    last day + homework

--- api/user.js
+++ api/user.js
@@ -6,7 +6,13 @@ export default ({ config, db }) => {
 
     //Create new user
 
-    //get all users that have 2 orders and more
+    //get all users that have 2 orders and more 
+    //AND at least one of the orders 
+    //has a product with qty >= 2
     //yassine myriam nadine
+    //fatma sellami
+
+    //
+
     return router
 }
